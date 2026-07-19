a613d24f06ef9e759bfeaacacc57d642d96640ec
kajolsinghme <kajolsingh985@gmail.com>
2026-07-19 11:02:38 +0530

    feat: integrate Tailwind CSS and update dependencies
    - Added Tailwind CSS and its Vite plugin to the project.
    - Removed App.css and migrated styles to Tailwind CSS classes.
    - Updated App.jsx to utilize Tailwind for styling and removed unnecessary state management.
    - Created new Login and Signup pages.
    - Updated index.css to import Tailwind CSS.
    - Updated vite.config.js to include Tailwind CSS plugin.

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,7 +22,7 @@ app.use("/api/auth", authRoutes);
 
 await connectDB();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- backend/src/validators/auth.validator.js
+++ backend/src/validators/auth.validator.js
@@ -3,7 +3,7 @@ import {z} from 'zod';
 export const signUpSchema = z.object({
     name: z.string().min(3, "Name is required"),
     email: z.string().email("Please enter a valid email"),
-    password: z.string().min(6, "Password must be at least 6 characters")
+    password: z.string().min(3, "Password must be at least 3 characters")
 })
 
 export const loginSchema = z.object({
